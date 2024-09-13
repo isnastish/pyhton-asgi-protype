263ec1fb92e88227ea9d949b9652595bd3a207a9
isnastish <paukden12@gmail.com>
2024-09-13 14:22:42 +0200

    added request start/end traces

--- app/app.py
+++ app/app.py
@@ -1,55 +1,81 @@
-# TODO: Ask Matias to explain about type-checking, 
-# in particular under which conditions TYPE_CHECKING constant is set to True 
 from typing import TYPE_CHECKING, Any
 from yarl import URL
+import logging
 
 if TYPE_CHECKING:
     from asgiref.typing import (
         Scope,
         HTTPScope, 
         ASGIReceiveCallable, 
-        ASGISendCallable
+        ASGISendCallable, 
+        LifespanScope, 
     )
 
-async def _handle_http_protocol(scope: "HTTPScope", receive: "ASGIReceiveCallable", send: "ASGISendCallable") -> None:
-    """Handle http calls"""
-    path = URL(scope["path"])
-    print(f"{path=}")
-    
-    await send({
-       "type": "http.response.start",
-       "status": 200,  
-       "headers": [
-           [b"content-type", b"text/plain"],
-       ]
-    })
-
-    await send({
-        "type": "http.response.body", 
-        "body": b"Hello World!"
-    })
-    
-
-async def _handle_lifespan_protocol(scope, receive, send) -> None:
-    """Handle lifespan calls"""
-
+_log = logging.getLogger(__name__)
 
 class App:
+    storage: dict[str, str] = {}
+
     def __init__(self) -> None:
         pass
 
+    async def _handle_http_protocol(self, scope: "HTTPScope", receive: "ASGIReceiveCallable", send: "ASGISendCallable") -> None:
+        """Handle http calls"""
+        method = scope["method"] 
+        if method not in ("PUT", "GET"):
+            raise RuntimeError(f"Unsupported method {scope['method']}")
+
+        if method == "GET": # get value from the storage 
+            url = scope["path"]
+            print(f"{url=}")
+
+            # This doesn't get an actual query string
+            query = scope["query_string"]
+            print(f"{query=}")
+            
+            await send({
+            "type": "http.response.start",
+            "status": 200,  
+            "headers": [
+                [b"content-type", b"text/plain"],
+            ]
+            })
+
+            await send({
+                "type": "http.response.body", 
+                "body": b"Hello World!",
+                "more_body": False,
+            })
+
+        elif method == "PUT": # put value into the storage
+            url = scope["path"]
+            print(f"{url=}")
+        
+
+    async def _handle_lifespan_protocol(self, scope: "LifespanScope", receive: "ASGIReceiveCallable", send: "ASGISendCallable") -> None:
+        """Handle lifespan calls"""
+        while True:
+            event = await receive()
+            if event["type"] == "lifespan.startup":
+               await send({"type": "lifespan.startup.complete"}) 
+            elif event["type"] == "lifespan.shutdown":
+                break
+                
+        await send({"type": "lifespan.shutdown.complete"})
+
+
     async def __call__(self, scope: "Scope", receive: "ASGIReceiveCallable", send: "ASGISendCallable") -> Any:
-        # TODO: Ask Matias why do we have to handle an exception while determining scope["type"]
-        # Why do we need to handle generic exception if we raise RuntimeError
         try:
             if scope["type"] == "lifespan":
-                await _handle_lifespan_protocol(scope, receive, send)
+                _log.info("lifespan protocol is used")
+                await self._handle_lifespan_protocol(scope, receive, send)
             elif scope["type"] == "http":
-                await _handle_http_protocol(scope, receive, send)
+                _log.info("http protocol is used")
+                await self._handle_http_protocol(scope, receive, send)
             else:
                 raise RuntimeError("Unknown ASGI protocol type", scope["type"])
             
-        except Exception as ex: # broad exception caught 
+        except Exception as ex:
             print(ex)
 
 app = App()
--- scripts/app_example.py
+++ scripts/app_example.py
@@ -1,20 +1,41 @@
 import aiohttp
 import asyncio
+from yarl import URL
 
-from aiohttp import TraceConfig 
+from aiohttp import TraceConfig  
 
-class AppClient:
+class Client:
     def __init__(self) -> None:
         pass 
 
-    
-async def main() -> None:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url="http://localhost:5051") as resp:
-            print("Status: ", resp.status)
-            # print("Content-type: ", resp.headers["content-type"])
+async def on_request_start(session, trace_ctx, params) -> None:
+    print("Request started")
 
-            body = await resp.read()
-            print("body: ", str(body))
+async def on_request_end(session, trace_ctx, params) -> None:
+    print("Request ended")
+
+async def on_request_chunk_sent(session, trace_ctx, params) -> None:
+    """"""
+
+async def on_request_chunk_received(session, trace_ctx, params) -> None:
+    """"""
+
+# TODO: Implement TraceConfig    
+trace_config = TraceConfig()
+trace_config.on_request_start.append(on_request_start)
+trace_config.on_request_end.append(on_request_end)
+
+async def main() -> None:
+    async with aiohttp.ClientSession(base_url="http://localhost:5051", trace_configs=[trace_config]) as session:
+        url = URL("/api/v1/storage/") 
+        url.with_query({"key": "value"})
+        async with session.get(url=url.path) as resp:
+            if resp.ok:
+                print("Status: ", resp.status)
+                body = await resp.read()
+                print("body: ", str(body))
+            else:
+                resp.raise_for_status() 
+            
     
 asyncio.run(main())
