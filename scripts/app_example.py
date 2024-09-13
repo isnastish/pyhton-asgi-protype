5bdd14cae999ce442697fbee4d09a8b4412cb9ac
isnastish <paukden12@gmail.com>
2024-09-13 15:07:38 +0200

    added Client class

--- scripts/app_example.py
+++ scripts/app_example.py
@@ -1,6 +1,8 @@
 import aiohttp
 import asyncio
 from yarl import URL
+from contextlib import AsyncExitStack
+from typing import Optional
 
 from aiohttp import (
     TraceConfig, 
@@ -8,9 +10,6 @@ from aiohttp import (
     TraceRequestChunkSentParams
 )  
 
-class Client:
-    def __init__(self) -> None:
-        pass 
 
 async def on_request_start(session, trace_ctx, params) -> None:
     print("Request started")
@@ -27,17 +26,44 @@ async def on_request_chunk_received(session, trace_ctx, params: TraceResponseChu
     print(f"received chunk {params.chunk}")
 
 
-# TODO: Implement TraceConfig    
-trace_config = TraceConfig()
-trace_config.on_request_start.append(on_request_start)
-trace_config.on_request_end.append(on_request_end)
+_trace_config = TraceConfig()
+_trace_config.on_request_start.append(on_request_start)
+_trace_config.on_request_end.append(on_request_end)
 
+
+class Client:
+    """Client session wrapper"""
+
+    def __init__(self, base_url: Optional[URL | str] = None) -> None:
+        self._base_url = URL(base_url) if base_url else URL("http://localhost:5051")
+        self._exit_stack: AsyncExitStack = None
+        self._session: aiohttp.ClientSession = None
+
+    async def __aenter__(self) -> aiohttp.ClientSession:
+        if self._exit_stack is not None:
+            raise RuntimeError("Exist stack already initialized")
+        
+        self._exit_stack = AsyncExitStack()
+        session = await self._exit_stack.enter_async_context(
+            aiohttp.ClientSession(base_url=self._base_url, trace_configs=[_trace_config])
+        )
+        self._session = session
+
+        return session
+
+    async def __aexit__(self, exc_type, exc, traceback):
+        if self._exit_stack:
+            await self._exit_stack.aclose()
+
+    @property
+    def base_url(self) -> str:
+        return self._base_url.path 
+    
 
 async def main() -> None:
-    async with aiohttp.ClientSession(base_url="http://localhost:5051", trace_configs=[trace_config]) as session:
+    async with Client() as client:
         url = URL("/api/v1/storage/") 
-        url.with_query({"key": "value"})
-        async with session.get(url=url.path) as resp:
+        async with client.get(url=url.path) as resp:
             if resp.ok:
                 print("Status: ", resp.status)
                 body = await resp.read()
@@ -45,5 +71,4 @@ async def main() -> None:
             else:
                 resp.raise_for_status() 
             
-    
 asyncio.run(main())
